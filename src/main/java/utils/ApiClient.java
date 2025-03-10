7f66f78e248f6c269d12076e132a8babfa4289e8
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-03-10 11:43:10 +0700

    fixing up gradle dependencies and create a class for base API request

---
+++ src/main/java/utils/ApiClient.java
@@ -0,0 +1,30 @@
+package utils;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+import java.util.Map;
+
+public class ApiClient {
+
+    public static Response get(String endpoint, Map<String, String> headers) {
+        return given()
+                .headers(headers)
+                .when()
+                .get(endpoint)
+                .then()
+                .extract()
+                .response();
+    }
+
+    public static Response post(String endpoint, Map<String, String> headers, Object body) {
+        return given()
+                .headers(headers)
+                .body(body)
+                .when()
+                .post(endpoint)
+                .then()
+                .extract()
+                .response();
+    }
+}
